5c97c5824a73582417b7a575a7f03838348611ef
JeremyNaicker <jeremy.n@towa.co.za>
2021-06-01 15:09:48 +0200

    Basket UI

--- NewShop/NewShop.WebUI/App_Start/UnityConfig.cs
+++ NewShop/NewShop.WebUI/App_Start/UnityConfig.cs
@@ -2,6 +2,7 @@ using NewShop.Core.Contracts;
 using NewShop.Core.Models;
 using NewShop.DataAccess.InMemory;
 using NewShop.DataAccess.SQL;
+using NewShop.Services;
 using System;
 
 using Unity;
@@ -48,6 +49,9 @@ namespace NewShop.WebUI
             // container.RegisterType<IProductRepository, ProductRepository>();
             container.RegisterType<IRepository<Product>, SQLRepository<Product>>();
             container.RegisterType<IRepository<ProductCategory>, SQLRepository<ProductCategory>>();
+            container.RegisterType<IRepository<Basket>, SQLRepository<Basket>>();
+            container.RegisterType<IRepository<BasketItem>, SQLRepository<BasketItem>>();
+            container.RegisterType<IBasketService, BasketService>();
         }
     }
 }
