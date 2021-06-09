4e4a7aa3cd4b03aedae177c6558ef7dc9aab9335
JeremyNaicker <jeremy.n@towa.co.za>
2021-06-09 14:45:08 +0200

    Order Managemnet

--- NewShop/NewShop.Services/OrderService.cs
+++ NewShop/NewShop.Services/OrderService.cs
@@ -34,5 +34,21 @@ namespace NewShop.Services
             OrderContext.Insert(baseOrder);
             OrderContext.Commit();
         }
+
+        public List<Order> GetOrderList()
+        {
+            return OrderContext.Collection().ToList();
+        }
+
+        public Order GetOrder(string Id)
+        {
+           return OrderContext.Find(Id);
+        }
+
+        public void UpdateOrder(Order updateorder)
+        {
+            OrderContext.Update(updateorder);
+            OrderContext.Commit();
+        }
     }
 }
